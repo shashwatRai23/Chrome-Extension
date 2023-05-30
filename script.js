a50980e3c23c67ccb999b26662debb7dd2683cfe
Shashwat <shashwat.rai.mec20@itbhu.ac.in>
2023-05-30 21:42:34 +0530

    added loader and modified the UI

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-const getContestData=async ()=>{
+const getContestData = async () => {
     return await fetch("https://kontests.net/api/v1/all")
-    .then((data)=>data.json())
-    .then((data)=> {
-        const relevantData=data.map((contest)=> `${contest.name} ${contest.site}`)
-        console.log({relevantData});
-        document.getElementById("contests").innerHTML=data.map(contest => 
-            `<div class="contest">
-            <div>${contest.name}</div>
-            <div class="site">
-                <div class="bold">${contest.site}</div>
-                <div>${contest.start_time}</div>
+      .then((data) => data.json())
+      .then((data) => {
+        const currentTime = new Date();
+        const upcomingContests = data
+          .filter((contest) => new Date(contest.start_time) > currentTime)
+          .sort((a, b) => new Date(a.start_time) - new Date(b.start_time));
+        const relevantData = upcomingContests.map((contest) => {
+          const startTime = new Date(contest.start_time);
+          const startTimeIST = startTime.toLocaleString("en-IN", {
+            timeZone: "Asia/Kolkata",
+          });
+          return `${contest.name} ${contest.site} ${startTimeIST}`;
+        });
+        console.log({ relevantData });
+        document.getElementById("contests").innerHTML = upcomingContests
+          .map((contest) => {
+            const startTime = new Date(contest.start_time);
+            const startTimeIST = startTime.toLocaleString("en-IN", {
+              timeZone: "Asia/Kolkata",
+            });
+            return `<div class="contest">
+            <div class="contest_site">
+                <div class="name">${contest.site}</div>
             </div>
-            <a href=${contest.url} class="url" target="_blank">Go to Contest</a>
-        </div>`).join(' ');
+            <div class="contest_name">${contest.name}</div>
+            <div class="site">
+                <a href=${contest.url} class="url" target="_blank">Go to Contest</a>
+                <div class="time">${startTimeIST}</div>
+            </div>
+        </div>`;
+          })
+          .join(" ");
         return relevantData;
-    })
-    .catch((e)=>console.log(e));
-}
-
-getContestData();
+      })
+      .catch((e) => console.log(e));
+  };
+  
+  getContestData();
+  
